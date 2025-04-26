bdc21ec17cae282df679d5b57257741011edbe8f
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2025-04-26 15:53:01 -0300

    Update Veiculo.java

--- JAVA ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
+++ JAVA ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -1,5 +1,10 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
-public class Veiculo {
 
+public class Veiculo {
+	private String placa;
+	private int ano;
+	private SeguradoEmpresa proprietarioEmpresa;
+	private SeguradoPessoa proprietarioPessoa;
+	private CategoriaVeiculo categoria;
 }
