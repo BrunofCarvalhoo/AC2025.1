c8a1912d3b3d960b458210bc0a607a878ca37d29
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2025-04-30 09:20:38 -0300

    update testes

--- JAVA ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteApoliceDAO.java
+++ JAVA ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteApoliceDAO.java
@@ -1,5 +1,100 @@
 package br.edu.cs.poo.ac.seguro.testes;
 
-public class TesteApoliceDAO {
+import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.edu.cs.poo.ac.seguro.daos.ApoliceDAO;
+import br.edu.cs.poo.ac.seguro.entidades.Apolice;
+import br.edu.cs.poo.ac.seguro.entidades.CategoriaVeiculo;
+import br.edu.cs.poo.ac.seguro.entidades.Veiculo;
+
+public class TesteApoliceDAO extends TesteDAO {
+    private ApoliceDAO dao = new ApoliceDAO();
+
+    @Override
+    protected Class<?> getClasse() {
+        return Apolice.class;
+    }
+
+    private Apolice criarApoliceComNumero(String numero) {
+        Veiculo veiculo = new Veiculo("PLACA" + numero, 2020, null, null, CategoriaVeiculo.BASICO);
+        Apolice apolice = new Apolice(veiculo, BigDecimal.valueOf(500), BigDecimal.valueOf(1000), BigDecimal.valueOf(50000));
+        apolice.setNumero(numero);
+        return apolice;
+    }
+
+    @Test
+    public void teste01() {
+        String numero = "00000001";
+        Apolice ap = criarApoliceComNumero(numero);
+        dao.incluir(ap);
+        Apolice buscada = dao.buscar(numero);
+        Assertions.assertNotNull(buscada);
+    }
+
+    @Test
+    public void teste02() {
+        String numero = "00000002";
+        dao.incluir(criarApoliceComNumero(numero));
+        Apolice ap = dao.buscar("99999999");
+        Assertions.assertNull(ap);
+    }
+
+    @Test
+    public void teste03() {
+        String numero = "00000003";
+        dao.incluir(criarApoliceComNumero(numero));
+        boolean ret = dao.excluir(numero);
+        Assertions.assertTrue(ret);
+    }
+
+    @Test
+    public void teste04() {
+        String numero = "00000004";
+        dao.incluir(criarApoliceComNumero(numero));
+        boolean ret = dao.excluir("00009999");
+        Assertions.assertFalse(ret);
+    }
+
+    @Test
+    public void teste05() {
+        String numero = "00000005";
+        Apolice ap = criarApoliceComNumero(numero);
+        boolean ret = dao.incluir(ap);
+        Assertions.assertTrue(ret);
+        Apolice buscada = dao.buscar(numero);
+        Assertions.assertNotNull(buscada);
+    }
+
+    @Test
+    public void teste06() {
+        String numero = "00000006";
+        Apolice ap = criarApoliceComNumero(numero);
+        dao.incluir(ap);
+        boolean ret = dao.incluir(ap);
+        Assertions.assertFalse(ret);
+    }
+
+    @Test
+    public void teste07() {
+        String numero = "00000007";
+        Apolice ap = criarApoliceComNumero(numero);
+        boolean ret = dao.alterar(ap);
+        Assertions.assertFalse(ret);
+        Apolice buscada = dao.buscar(numero);
+        Assertions.assertNull(buscada);
+    }
+
+    @Test
+    public void teste08() {
+        String numero = "00000008";
+        Apolice ap = criarApoliceComNumero(numero);
+        dao.incluir(ap);
+        Apolice alterada = criarApoliceComNumero(numero); 
+        alterada.setValorPremio(BigDecimal.valueOf(2000));
+        boolean ret = dao.alterar(alterada);
+        Assertions.assertTrue(ret);
+    }
 }
--- JAVA ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteSinistroDAO.java
+++ JAVA ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteSinistroDAO.java
@@ -1,5 +1,109 @@
 package br.edu.cs.poo.ac.seguro.testes;
 
-public class TesteSinistroDAO {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.edu.cs.poo.ac.seguro.daos.SinistroDAO;
+import br.edu.cs.poo.ac.seguro.entidades.CategoriaVeiculo;
+import br.edu.cs.poo.ac.seguro.entidades.Sinistro;
+import br.edu.cs.poo.ac.seguro.entidades.TipoSinistro;
+import br.edu.cs.poo.ac.seguro.entidades.Veiculo;
+
+public class TesteSinistroDAO extends TesteDAO {
+    private SinistroDAO dao = new SinistroDAO();
+
+    @Override
+    protected Class<?> getClasse() {
+        return Sinistro.class;
+    }
+
+    private Sinistro criarSinistroComNumero(String numero) {
+        Veiculo veiculo = new Veiculo("PLACA" + numero, 2021, null, null, CategoriaVeiculo.INTERMEDIARIO);
+        Sinistro sinistro = new Sinistro(
+            veiculo,
+            LocalDateTime.now().minusDays(2),
+            LocalDateTime.now(),
+            "usuario" + numero,
+            BigDecimal.valueOf(3000),
+            TipoSinistro.COLISAO
+        );
+        sinistro.setNumero(numero);
+        return sinistro;
+    }
+
+    @Test
+    public void teste01() {
+        String numero = "S0001";
+        Sinistro sin = criarSinistroComNumero(numero);
+        dao.incluir(sin);
+        Sinistro buscado = dao.buscar(numero);
+        Assertions.assertNotNull(buscado);
+    }
+
+    @Test
+    public void teste02() {
+        String numero = "S0002";
+        dao.incluir(criarSinistroComNumero(numero));
+        Sinistro sin = dao.buscar("S9999");
+        Assertions.assertNull(sin);
+    }
+
+    @Test
+    public void teste03() {
+        String numero = "S0003";
+        dao.incluir(criarSinistroComNumero(numero));
+        boolean ret = dao.excluir(numero);
+        Assertions.assertTrue(ret);
+    }
+
+    @Test
+    public void teste04() {
+        String numero = "S0004";
+        dao.incluir(criarSinistroComNumero(numero));
+        boolean ret = dao.excluir("S0005");
+        Assertions.assertFalse(ret);
+    }
+
+    @Test
+    public void teste05() {
+        String numero = "S0005";
+        Sinistro sin = criarSinistroComNumero(numero);
+        boolean ret = dao.incluir(sin);
+        Assertions.assertTrue(ret);
+        Sinistro buscado = dao.buscar(numero);
+        Assertions.assertNotNull(buscado);
+    }
+
+    @Test
+    public void teste06() {
+        String numero = "S0006";
+        Sinistro sin = criarSinistroComNumero(numero);
+        dao.incluir(sin);
+        boolean ret = dao.incluir(sin);
+        Assertions.assertFalse(ret);
+    }
+
+    @Test
+    public void teste07() {
+        String numero = "S0007";
+        Sinistro sin = criarSinistroComNumero(numero);
+        boolean ret = dao.alterar(sin);
+        Assertions.assertFalse(ret);
+        Sinistro buscado = dao.buscar(numero);
+        Assertions.assertNull(buscado);
+    }
+
+    @Test
+    public void teste08() {
+        String numero = "S0008";
+        Sinistro sin = criarSinistroComNumero(numero);
+        dao.incluir(sin);
+        Sinistro alterado = criarSinistroComNumero(numero);
+        alterado.setValorSinistro(BigDecimal.valueOf(9000));
+        boolean ret = dao.alterar(alterado);
+        Assertions.assertTrue(ret);
+    }
 }
