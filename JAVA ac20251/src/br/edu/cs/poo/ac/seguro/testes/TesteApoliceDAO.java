e0047d8f31fff8cb545e4a3955d9cd473c625934
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2025-05-04 16:02:54 -0300

    .

--- JAVA ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteApoliceDAO.java
+++ JAVA ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteApoliceDAO.java
@@ -44,12 +44,8 @@ public class TesteApoliceDAO extends TesteDAO {
     @Test
     public void teste02() {
 
-        String numero = "0";
-
         boolean ret = dao.alterar(new Apolice("0", veiculo, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, LocalDate.now()));
         Assertions.assertFalse(ret);
-
-
     }
 
     @Test
@@ -58,24 +54,15 @@ public class TesteApoliceDAO extends TesteDAO {
         String numero = "0";
 
         cadastro.incluir(new Apolice("0", veiculo, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, LocalDate.now()),numero);
-        Apolice seg = dao.buscar(numero);
         boolean ret = dao.excluir(numero);
         Assertions.assertTrue(ret);
-
-
-
-
-
     }
 
     @Test
     public void teste04() {
         String numero = "0";
 
-        cadastro.incluir(
-                new Apolice("0", veiculo, new BigDecimal("330.00"), new BigDecimal("500.00"),
-                        new BigDecimal("5000.00"), LocalDate.now()), numero);
-
+        cadastro.incluir(new Apolice("0", veiculo, new BigDecimal("330.00"), new BigDecimal("500.00"),new BigDecimal("5000.00"), LocalDate.now()), numero);
         boolean ret = dao.excluir("10");
         Assertions.assertFalse(ret);
     }
@@ -83,18 +70,9 @@ public class TesteApoliceDAO extends TesteDAO {
     @Test
     public void teste05() {
         String numero = "10";
-        Apolice apolice = new Apolice(
-                "1099",
-                veiculo,
-                new BigDecimal("320.00"),
-                new BigDecimal("400.00"),
-                new BigDecimal("523320.00"),
-                LocalDate.now()
-        );
-
+        Apolice apolice = new Apolice("1099",veiculo,new BigDecimal("320.00"), new BigDecimal("400.00"),new BigDecimal("523320.00"),LocalDate.now());
         apolice.setNumero(numero);
         boolean ret = dao.incluir(apolice);
-
         Assertions.assertTrue(ret);
         Apolice apo = dao.buscar(numero);
         Assertions.assertNotNull(apo);
@@ -104,15 +82,7 @@ public class TesteApoliceDAO extends TesteDAO {
     public void teste06() {
         String numero = "0";
 
-        Apolice apo = new Apolice(
-                "999",
-                veiculo,
-                new BigDecimal("10000.00"),
-                new BigDecimal("4000.00"),
-                new BigDecimal("60000.00"),
-                LocalDate.now()
-        );
-
+        Apolice apo = new Apolice( "999", veiculo, new BigDecimal("10000.00"), new BigDecimal("4000.00"), new BigDecimal("60000.00"), LocalDate.now());
         apo.setNumero(numero);
         cadastro.incluir(apo, numero);
         boolean ret = dao.incluir(apo);
@@ -122,14 +92,7 @@ public class TesteApoliceDAO extends TesteDAO {
     @Test
     public void teste07() {
         String numero = "0";
-        Apolice apolice = new Apolice(
-                numero,
-                veiculo,
-                new BigDecimal("4000.00"),
-                new BigDecimal("900.00"),
-                new BigDecimal("50000.00"),
-                LocalDate.now()
-        );
+        Apolice apolice = new Apolice(numero, veiculo, new BigDecimal("4000.00"), new BigDecimal("900.00"), new BigDecimal("50000.00"), LocalDate.now());
         boolean ret = dao.alterar(apolice);
         Assertions.assertFalse(ret);
         Apolice apo = dao.buscar(numero);
@@ -140,24 +103,9 @@ public class TesteApoliceDAO extends TesteDAO {
     public void teste08() {
         String numero = "0";
 
-        Apolice apo = new Apolice(
-                numero,
-                veiculo,
-                new BigDecimal("10000.00"),
-                new BigDecimal("1000.00"),
-                new BigDecimal("100000.00"),
-                LocalDate.now()
-        );
+        Apolice apo = new Apolice( numero,veiculo,new BigDecimal("10000.00"),new BigDecimal("1000.00"),new BigDecimal("100000.00"),LocalDate.now());
         cadastro.incluir(apo, numero);
-
-        apo = new Apolice(
-                numero,
-                veiculo,
-                new BigDecimal("7000.00"),
-                new BigDecimal("1000.00"),
-                new BigDecimal("70000.00"),
-                LocalDate.now()
-        );
+        apo = new Apolice(numero,veiculo,new BigDecimal("7000.00"), new BigDecimal("1000.00"), new BigDecimal("70000.00"), LocalDate.now());
 
         boolean ret = dao.alterar(apo);
         Assertions.assertTrue(ret);
